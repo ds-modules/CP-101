9170a3b28e768a1f7ec12a8dcdbf5adb688f55e7
Monica Wilkinson <m.wilkinson@berkeley.edu>
2019-03-10 12:06:40 -0700

    add twitter api utility functions

---
+++ lab_9/twitter.py
@@ -0,0 +1,119 @@
+""" Utility functions for City Planning 101 Twitter API lab.
+
+These functions have been adapted from Data 100 course materials.
+"""
+
+def load_keys(path):
+    """Loads your Twitter authentication keys from a file on disk.
+    
+    Args:
+        path (str): The path to your key file.  The file should
+          be in JSON format and look like this (but filled in):
+            {
+                "consumer_key": "<your Consumer Key here>",
+                "consumer_secret":  "<your Consumer Secret here>",
+                "access_token": "<your Access Token here>",
+                "access_token_secret": "<your Access Token Secret here>"
+            }
+    
+    Returns:
+        dict: A dictionary mapping key names (like "consumer_key") to
+          key values."""
+    
+    import json
+    with open(path) as f:
+        keys = json.load(f)
+    return keys
+
+def download_recent_tweets_by_user(user_account_name, keys):
+    """Downloads tweets by one Twitter user.
+
+    Args:
+        user_account_name (str): The name of the Twitter account
+          whose tweets will be downloaded.
+        keys (dict): A Python dictionary with Twitter authentication
+          keys (strings), like this (but filled in):
+            {
+                "consumer_key": "<your Consumer Key here>",
+                "consumer_secret":  "<your Consumer Secret here>",
+                "access_token": "<your Access Token here>",
+                "access_token_secret": "<your Access Token Secret here>"
+            }
+
+    Returns:
+        list: A list of Dictonary objects, each representing one tweet."""
+    import tweepy
+    try:
+        auth = tweepy.OAuthHandler(keys["consumer_key"], keys["consumer_secret"])
+        auth.set_access_token(keys["access_token"], keys["access_token_secret"])
+        api = tweepy.API(auth)
+        tweets = [t._json for t in tweepy.Cursor(api.user_timeline, id=user_account_name, 
+                                             tweet_mode='extended').items()]
+    except TweepError as e:
+        logging.warning("There was a Tweepy error. Double check your API keys and try again.")
+        logging.warning(e)
+    return tweets
+
+def save_tweets(tweets, path):
+    """Saves a list of tweets to a file in the local filesystem.
+    
+    This function makes no guarantee about the format of the saved
+    tweets, **except** that calling load_tweets(path) after
+    save_tweets(tweets, path) will produce the same list of tweets
+    and that only the file at the given path is used to store the
+    tweets.  (That means you can implement this function however
+    you want, as long as saving and loading works!)
+
+    Args:
+        tweets (list): A list of tweet objects (of type Dictionary) to
+          be saved.
+        path (str): The place where the tweets will be saved.
+
+    Returns:
+        None"""
+    with open(path, "w") as f:        
+        json.dump(tweets, f)
+        
+def load_tweets(path):
+    """Loads tweets that have previously been saved.
+    
+    Calling load_tweets(path) after save_tweets(tweets, path)
+    will produce the same list of tweets.
+    
+    Args:
+        path (str): The place where the tweets were be saved.
+
+    Returns:
+        list: A list of Dictionary objects, each representing one tweet."""
+    with open(path, "r") as f:
+        tweets = json.load(f)
+    return tweets
+
+def get_tweets_with_cache(user_account_name, keys_path):
+    """Get recent tweets from one user, loading from a disk cache if available.
+    
+    The first time you call this function, it will download tweets by
+    a user.  Subsequent calls will not re-download the tweets; instead
+    they'll load the tweets from a save file in your local filesystem.
+    All this is done using the functions you defined in the previous cell.
+    This has benefits and drawbacks that often appear when you cache data:
+    
+    +: Using this function will prevent extraneous usage of the Twitter API.
+    +: You will get your data much faster after the first time it's called.
+    -: If you really want to re-download the tweets (say, to get newer ones,
+       or because you screwed up something in the previous cell and your
+       tweets aren't what you wanted), you'll have to find the save file
+       (which will look like <something>_recent_tweets.pkl) and delete it.
+    
+    Args:
+        user_account_name (str): The Twitter handle of a user, without the @.
+        keys_path (str): The path to a JSON keys file in your filesystem.
+    """
+    ds_tweets_save_path = '{}_recent_tweets.pkl'.format(user_account_name)
+    if not Path(ds_tweets_save_path).is_file():
+        tweets = download_recent_tweets_by_user(user_account_name, load_keys(keys_path))
+        save_tweets(tweets, ds_tweets_save_path)
+        
+    return load_tweets(ds_tweets_save_path)
+
+
